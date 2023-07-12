069889092ef06df518d62427b5a0f0428884f612
rover1983 <rover1983@yahoo.com>
2023-07-12 19:21:14 +0300

    updating

---
+++ src/test/java/tests/TestSuit1.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestSuit1 {
+
+WebDriver driver;
+	
+	@BeforeTest
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void testBox() {
+		driver.get("http://seleniumhq.org");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+	}
+
+	
+}
